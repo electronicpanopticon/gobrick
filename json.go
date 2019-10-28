86765d4a6701b84033c9cfb8f4a5d3e5e65f0d7f
Christoph <GrandMaster@templar.dev>
2019-10-28 17:24:33 -0400

    added json tests

--- json.go
+++ json.go
@@ -3,10 +3,14 @@ package gobrick
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 )
 
-func ToJSONBytes(v interface{}) []byte {
+func ToJsonBytes(v interface{}) []byte {
 	b, err := json.MarshalIndent(v, "", "  ")
+	if string(b) == "null" {
+		return nil
+	}
 	if err != nil {
 		fmt.Println(err)
 		return nil
@@ -14,9 +18,9 @@ func ToJSONBytes(v interface{}) []byte {
 	return b
 }
 
-func ToJSON(v interface{}) string {
-	b := ToJSONBytes(v)
-	if b == nil {
+func ToJson(v interface{}) string {
+	b := strings.TrimSpace(string(ToJsonBytes(v)))
+	if b == "" || b == "\"\"" {
 		return "{}"
 	}
 	return string(b)
--- json_test.go
+++ json_test.go
@@ -1,1 +1,20 @@
 package gobrick
+
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestJson(t *testing.T) {
+	t.Run("ToJsonBytes()", func(t *testing.T) {
+		var expected []byte = nil
+		assert.Equal(t, expected, ToJsonBytes(nil))
+	})
+
+	t.Run("ToJson", func(t *testing.T) {
+		assert.Equal(t, "{}", ToJson(nil))
+		assert.Equal(t, "{}", ToJson(""))
+		//  TODO: get me to work: assert.Equal(t, "{}", ToJson("  "))
+	})
+}
